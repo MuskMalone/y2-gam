59e5cb58f1a8a2754c4421f09be6fb6df815ea71
xavier <xav.choa@gmail.com>
2023-09-20 15:34:36 +0800

    reimplemented framebuffer

--- y2-gam-engine/include/Graphics/Framebuffer.hpp
+++ y2-gam-engine/include/Graphics/Framebuffer.hpp
@@ -1,31 +1,28 @@
 #pragma once
-#include "memory"
+#include <memory>
 
 struct FramebufferProps {
 	unsigned int width, height;
-	unsigned int samples{ 1 };
 
-	bool swapChainTarget{ false }; //rendering to screen or not
 };
 
 class Framebuffer {
 public:
-	Framebuffer(FramebufferProps const& props);
+	Framebuffer(FramebufferProps const&);
 	~Framebuffer();
-
-	static std::shared_ptr<Framebuffer> Create(FramebufferProps const& props);
-
-	unsigned int GetColorAttachmentID() const;
-	FramebufferProps const& GetProps() const;
 
 	void Invalidate();
 
-	void Bind();
-	void Unbind();
+	void Bind() const;
+	void Unbind() const;
 
+	unsigned int GetColorAttachmentID() const;
+	FramebufferProps const& GetFramebufferProps() const;
+
+	static std::shared_ptr<Framebuffer> Create(FramebufferProps const& props);
 private:
-	unsigned int mFboHdl{};
-	unsigned int mColorAttachment{};
-	unsigned int mDepthAttachment{};
+	unsigned int mFboHdl;
+	unsigned int mColorAttachment;
+	unsigned int mDepthAttachment;
 	FramebufferProps mProps;
 };
