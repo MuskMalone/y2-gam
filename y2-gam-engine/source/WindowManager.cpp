d227fb5e030bc77f22fdc97b2293d7ea0cf3b148
xavier <xav.choa@gmail.com>
2023-11-19 19:37:41 +0800

    Changed back key to F11

--- y2-gam-engine/source/WindowManager.cpp
+++ y2-gam-engine/source/WindowManager.cpp
@@ -115,7 +115,7 @@ void WindowManager::KeyCb(GLFWwindow* pwin, int key, int scancode, int action, i
 #endif
 			gCoordinator->SendEvent(event);
 		}
-		if (GLFW_KEY_0 == key) {
+		if (GLFW_KEY_F11 == key) {
 			Event e {Events::Window::TOGGLE_FULLSCREEN};
 			gCoordinator->SendEvent(e);
 		}
@@ -264,7 +264,7 @@ void WindowManager::FullscreenListener(Event const& event) {
 		mIsFullscreen = true;
 	}
 	else {
-		int safePosY = std::max(mWindowedPosY, 30); // Adjust 30 or to a suitable offset
+		int safePosY = std::max(mWindowedPosY, 30); //ensures titlebar doesnt go offscreen
 		glfwSetWindowMonitor(mWindow, nullptr, mWindowedPosX, safePosY, mWindowedWidth, mWindowedHeight, 0);
 
 		mIsFullscreen = false;
