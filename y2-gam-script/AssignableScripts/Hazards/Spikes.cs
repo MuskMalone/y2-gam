bce74e0b0f880eca2a48cf95484de2deb6eb26d0
beepbeepbooboob <ngwenwen02@gmail.com>
2023-11-24 11:15:14 +0800

    Revert "latest update"
    This reverts commit ad403c577a50f28965c7a840222834dac71000e4.

--- y2-gam-script/AssignableScripts/Hazards/Spikes.cs
+++ y2-gam-script/AssignableScripts/Hazards/Spikes.cs
@@ -1,24 +1,27 @@
-﻿using Image;
-using System;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
+﻿//using Image;
+//using System;
 
-namespace Object
-{
-    public class Spike : Entity
-    {
+//namespace Object
+//{
+//    public class Spike : Entity
+//    {
 
-        public Spike() : base()
-        {
+//        public Spike() : base()
+//        {
 
-        }
+//        }
 
-        public Spike(uint entityHandle) : base(entityHandle)
-        {
-            entityID = entityHandle;
-        }
+//        public Spike(uint entityHandle) : base(entityHandle)
+//        {
+//            entityID = entityHandle;
+//        }
 
-    }
+//        public void DamagePlayer(Player player)
+//        {
+//            player.PlayerTakeDamage();
+//        }
+       
 
-   
+//    }
 
-}
+//}
--- y2-gam-script/Player/Player.cs
+++ y2-gam-script/Player/Player.cs
@@ -17,7 +17,6 @@
 
 using Image;
 using System;
-using System.Dynamic;
 
 namespace Object
 {
@@ -165,15 +164,11 @@ namespace Object
 
         public void PlayerTakeDamage()
         {
-            PhysicsWrapper.Raycast(new Vector2(enemy.Translation.X, enemy.Translation.Y), attackRayEnd, enemy.entityID, out RaycastHit damageRayCast);
-           
             Health -= 1;
             if(Health <= 0)
             {
                 Console.WriteLine("Player died");
             }
         }
-
-
     }
 }
