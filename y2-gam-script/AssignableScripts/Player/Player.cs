a03739a46980f805e2c80ff4ee52b76b71f11bb5
beepbeepbooboob <ngwenwen02@gmail.com>
2024-02-02 12:17:07 +0800

    Fixes
    - fix the anvil and box moving
    - remove hole
    - change controls
    - can be killed from own anvil

--- y2-gam-script/AssignableScripts/Player/Player.cs
+++ y2-gam-script/AssignableScripts/Player/Player.cs
@@ -34,6 +34,7 @@ namespace Object
         public Vector2 spawnPosition = new Vector2(-400, -27);
         public Vector2 colliderPosition = new Vector2(-400, -36);
 
+        private Vector2 playerHead;
         private float temp_dt = 0f;
         private bool isPaused = false;
 
@@ -177,7 +178,7 @@ namespace Object
                         Mass = 0;
                     }
 
-                    if (Input.IsKeyPressed(KeyCode.KEY_SPACE))
+                    if (Input.IsKeyPressed(KeyCode.KEY_LEFT_SHIFT))
                     {
                         if (!IsKeyPressed)
                         {
@@ -233,6 +234,21 @@ namespace Object
                         Respawn();
                     }
                     */
+                    if (isFacingRight)
+                    {
+                        // Cast the ray from the right side of the head
+                        playerHead = new Vector2(Collider.X, Collider.Y - (Scale.X / 3.0f));
+                    }
+                    else
+                    {
+                        // Cast the ray from the left side of the head
+                        playerHead = new Vector2(Collider.X, Collider.Y + (Scale.X / 3.0f));
+                    }
+
+                    if (PhysicsWrapper.Raycast(Collider, playerHead, entityID, out RaycastHit anvilHit) && anvilHit.tag == "Anvil")
+                    {
+                        Respawn();
+                    }
 
                     if (Translation.Y <= -99.0f)
                     {
