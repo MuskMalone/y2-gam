66bbb775fab477a8eb2bf60f76d6a0fe09c1a89a
beepbeepbooboob <ngwenwen02@gmail.com>
2024-03-05 02:48:06 +0800

    menu button sound

--- y2-gam-script/AssignableScripts/Buttons/EnterLevelOne.cs
+++ y2-gam-script/AssignableScripts/Buttons/EnterLevelOne.cs
@@ -87,6 +87,7 @@ namespace Object
 
             if (UIClicked && firstTime)
             {
+                PlayAudio("menu_click.wav", 0);
                 StopAudio();
                 LoadScene("Cutscene");
                 firstTime = false;
